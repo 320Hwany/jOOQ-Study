7153bc2ed819694f75d9f93569d4ff1a3436ac00
Jung-YouHwan <yhwjd99@gmail.com>
2024-07-22 17:17:12 +0900

    feat : 초기 설정

---
+++ src/main/java/jooqstudy/JOoqStudyApplication.java
@@ -0,0 +1,13 @@
+package jooqstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JOoqStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JOoqStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jooqstudy/JOoqStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jooqstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JOoqStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
